189bd44cdb2bb20f02a2b9012951c5d4ceb199f5
Abdur-P-Rahim <bspirahim@gmail.com>
2023-03-29 03:50:04 +0600

    navber added

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -5,15 +5,17 @@ import logo from '../../images/Logo.svg'
 
 const Header = () => {
     return (
-        <div className='header'>
-           <img src={logo} alt="" />
+        <nav className='header'>
+          <div>
+          <img src={logo} alt="" />
+          </div>
            <div>
              <a href="/shop">Shop</a>
              <a href="/order">Order</a>
              <a href="/inventory">Inventory</a>
              <a href="login">Login</a>
            </div>
-        </div>
+        </nav>
     );
 };
 
